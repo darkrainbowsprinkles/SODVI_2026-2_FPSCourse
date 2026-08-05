7ca1da46ed758568ebae1ea59921c7ccd1a52b17
Octavio Ortega <octaviortegan@hotmail.com>
2026-08-05 08:47:05 -0600

    Enemy attack animation and behavior

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -7,23 +7,43 @@ namespace FPS.Control
     {
         [SerializeField, Range(0,1)] float chaseSpeedFraction = 0.5f;
         [SerializeField] float chaseRange = 10f;
+        [SerializeField] float attackRange = 2f;
         GameObject player;
         Mover mover;
+        Animator animator;
 
         void Awake()
         {
             player = GameObject.FindWithTag("Player");
             mover = GetComponent<Mover>();
+            animator = GetComponent<Animator>();
         }
 
         void Update()
         {
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
-            if (distanceToPlayer < chaseRange)
+            if (distanceToPlayer < attackRange)
             {
+                mover.Stop();
+                animator.SetTrigger("attack");
+                mover.LookAt(player);
+            }
+            else if (distanceToPlayer < chaseRange)
+            {
+                animator.ResetTrigger("attack");
                 mover.MoveTo(player.transform.position, chaseSpeedFraction);
             }
+            else
+            {
+                mover.Stop();
+            }
+        }
+
+        // Called in Unity Events
+        void Hit()
+        {
+            print("HIT!!!");
         }
 
         void OnDrawGizmosSelected()
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -5,9 +5,11 @@ namespace FPS.Movement
 {
     public class Mover : MonoBehaviour
     {
-        [SerializeField] float maxSpeed = 10;
+        [SerializeField] float maxSpeed = 10f;
+        [SerializeField] float rotationSpeed = 10f;
         CharacterController controller;
         NavMeshAgent agent;
+        Animator animator;
 
         public void MoveTo(Vector3 destination, float speedFraction)
         {
@@ -25,6 +27,18 @@ namespace FPS.Movement
             }
         }
 
+        public void LookAt(GameObject target)
+        {
+            Vector3 lookDirection = target.transform.position - transform.position;
+            lookDirection.y = 0f;
+
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation, 
+                Quaternion.LookRotation(lookDirection), 
+                rotationSpeed * Time.deltaTime
+            );
+        }
+
         public void Stop()
         {
             agent.isStopped = true;
@@ -34,6 +48,18 @@ namespace FPS.Movement
         {
             controller = GetComponent<CharacterController>();
             agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (animator == null)
+            {
+                return;
+            }
+
+            float localVelocity = transform.InverseTransformDirection(agent.velocity).magnitude;
+            animator.SetFloat("movementSpeed", localVelocity);
         }
     }
 }
